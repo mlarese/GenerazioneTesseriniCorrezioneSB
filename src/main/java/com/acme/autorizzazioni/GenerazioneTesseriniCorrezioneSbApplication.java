9912b3cfe0b5adc336214f67f482d77e4d29b9b5
Larese Moro Mauro <mauro.larese@gmail.com>
2022-08-09 15:35:50 +0200

    a

--- src/main/java/com/acme/autorizzazioni/GenerazioneTesseriniCorrezioneSbApplication.java
+++ src/main/java/com/acme/autorizzazioni/GenerazioneTesseriniCorrezioneSbApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//messo in branch
+//messo in branchs
 public class GenerazioneTesseriniCorrezioneSbApplication {
 	// fatto in main
 	public static void main(String[] args) {
