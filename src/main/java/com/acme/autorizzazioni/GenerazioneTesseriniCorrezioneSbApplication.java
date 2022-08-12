66e8e4c47144d11c80e8b29eee659f82a44d7d3d
Larese Moro Mauro <mauro.larese@gmail.com>
2022-08-12 10:10:08 +0200

    test per il revert di un push

--- src/main/java/com/acme/autorizzazioni/GenerazioneTesseriniCorrezioneSbApplication.java
+++ src/main/java/com/acme/autorizzazioni/GenerazioneTesseriniCorrezioneSbApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//messo in branch
+//messo in branch voglio revertare un push
 public class GenerazioneTesseriniCorrezioneSbApplication {
 	// fatto in main
 	public static void main(String[] args) {
